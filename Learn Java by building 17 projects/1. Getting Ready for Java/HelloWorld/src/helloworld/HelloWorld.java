8f56c2c5ad323327a0c5712165c8f978f74790d7
SAPTARSHI DAS <75030944+Saptarshidas131@users.noreply.github.com>
2021-01-04 23:04:19 +0530

    Add files via upload

---
+++ Learn Java by building 17 projects/1. Getting Ready for Java/HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+/**
+ *
+ * @author saptarshi
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
